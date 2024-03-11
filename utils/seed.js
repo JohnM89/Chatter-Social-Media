840f61aaec3c028369501e74c203c5a5608bb2b5
JohnM89 <mjammer2@gmail.com>
2024-03-10 21:34:54 -0400

    added seeding function to create a random number of friends for each user without self friending or duplications

--- utils/seed.js
+++ utils/seed.js
@@ -4,7 +4,7 @@ const { User, Thought } = require('../models');
 const { reaction, user, thought, getRandomArrItem,  } = require('./data');
 
 
-// Function to generate a random email based on username
+// generate a random email based on username
 const generateEmail = (username) => {
   const cleanedUsername = username.replace(/\s+/g, '').toLowerCase();
   return `${cleanedUsername}@example.com`;
@@ -25,20 +25,21 @@ connection.once('open', async () => {
     email: generateEmail(username)
   }));
 
-  // Insert users into the database
+  // inserts users into the database
   const createdUsers = await User.create(users);
   console.log('Users inserted');
 
-  // Create thoughts for each user
+  // create thoughts for each user
   const thoughtsData = [];
 
+// create a random number of thoughts for each user
   createdUsers.forEach(user => {
-    const numThoughts = Math.floor(Math.random() * 5) + 1; // Random number of thoughts per user
+    const numThoughts = Math.floor(Math.random() * 5) + 1; 
     for (let i = 0; i < numThoughts; i++) {
     thoughtsData.push({
       thoughtText: getRandomArrItem(thought),
       username: user._id,
-      reactions: [{ // This assumes each thought starts with one reaction for simplification
+      reactions: [{ 
         reactionBody: getRandomArrItem(reaction),
         username: user.username,
         reactionId: new mongoose.Types.ObjectId()
@@ -47,15 +48,31 @@ connection.once('open', async () => {
   }
 });
 
-  // Insert thoughts data into the database
-const createdThoughts = await Thought.create(thoughtsData);
-console.log('Thoughts inserted');
+// create a random number of friends for each user
+  createdUsers.forEach(user => {
+    const numFriends = Math.floor(Math.random() * (createdUsers.length - 1)) + 1;
+    const friendIds = [];
+    while(friendIds.length < numFriends){
+      const potentialFriendId = createdUsers[Math.floor(Math.random() * createdUsers.length)]._id;
+      // no self friending and no duplicate friending
+      if (potentialFriendId.toString() !== user._id.toString() && !friendIds.includes(potentialFriendId)) {
+        friendIds.push(potentialFriendId);
+      }
+    }
+    User.findByIdAndUpdate(user._id, { $push: { friends: { $each: friendIds } } });
+  });
 
-await Promise.all(createdUsers.map(async (user) => {
-  const userThoughts = createdThoughts.filter(thought => thought.username.toString() === user._id.toString());
-  const thoughtIds = userThoughts.map(thought => thought._id);
-  await User.findByIdAndUpdate(user._id, { $push: { thoughts: { $each: thoughtIds } } });
-}));
+
+  // puts thoughts data into the database
+  const createdThoughts = await Thought.create(thoughtsData);
+  console.log('Thoughts inserted');
+
+ // promise.all to push thought id to user
+  await Promise.all(createdUsers.map(async (user) => {
+    const userThoughts = createdThoughts.filter(thought => thought.username.toString() === user._id.toString());
+    const thoughtIds = userThoughts.map(thought => thought._id);
+    await User.findByIdAndUpdate(user._id, { $push: { thoughts: { $each: thoughtIds } } });
+  }));
 
   console.info('Seeding complete! 🌱');
   process.exit(0);
